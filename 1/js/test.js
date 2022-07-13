cd8680d75000e69df41200250fbc8fe941b9882e
home <home@gmail.com>
2022-07-13 17:07:28 +0300

    create movie db

--- 1/js/test.js
+++ 1/js/test.js
@@ -3,6 +3,29 @@ function useStrict() {
     "use strict";
 }
 
-const category = 'toys';
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "0");
+console.log(numberOfFilms);
+const personalMovieDB = {
+    numberOfFilms:numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat: false
+}
 
-console.log(`https://someurl.com/${category}/5`);
+const movieName1 = prompt("Один из последних просмотренных фильмов?", "");
+const rate1 = prompt("На сколько оцените его?", "0");
+const movieName2 = prompt("Один из последних просмотренных фильмов?", "");
+const rate2 = prompt("На сколько оцените его?", "1");
+
+personalMovieDB.movies = {
+    movieName1: rate1,
+    movieName2: rate2
+};
+
+console.log(personalMovieDB.movies);
+
+
+
+
+
