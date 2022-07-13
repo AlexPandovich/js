9aba2bb590aaae335cb9e7a0974e72a3060eacd3
home <home@gmail.com>
2022-07-13 20:42:42 +0300

    compleate conditions

--- 1/js/course.js
+++ 1/js/course.js
@@ -1,6 +1,18 @@
 "use strict";
 
-let num = 44;
+let num = 50;
 
 let result = (num === 50) ? true : false;
 console.log(result);
+
+switch(num) {
+    case 50:
+        console.log("правдиво");
+        break;
+    case 49:
+        console.log("Неверно");
+        break;
+    default:
+        console.log("дефолт");
+        break;
+}
