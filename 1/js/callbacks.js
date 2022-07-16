32851d39c3d2108961937bd2ed0731bf2b5a8c71
home <home@gmail.com>
2022-07-16 13:00:51 +0300

    compleate object destruction

--- 1/js/callbacks.js
+++ 1/js/callbacks.js
@@ -28,3 +28,5 @@ learnJs("javascript", function() {
 
 
 
+
+
--- 1/js/objects.js
+++ 1/js/objects.js
@@ -1,2 +1,38 @@
 "use strict";
+const obj = {};
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+
+//console.log(options.name);
+//delete options.height;
+//console.log(options);
+
+for(let i in options) {
+    if(typeof(options[i]) == 'object') {
+        for(let j in options[i]) {
+            console.log(`Свойтство ${j} имеет значение ${options[i][j]}`);
+        }
+    } else {
+        console.log(`Свойтство ${i} имеет значение ${options[i]}`);
+    }
+    
+}
+
+//console.log(Object.keys(options).length);
+options.makeTest();
+
+const {border, bg} = options.colors;
+
+console.log(border);
 
